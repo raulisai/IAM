c84c650c34dbe9ba47de75e5700a1a78bc71f6eb
raul monroy <raulisai97@gmail.com>
2025-10-13 17:49:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/iam/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.iam
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WebViewPage(url = "https://www.google.com")
+        }
+    }
+}
+
+@Composable
+fun WebViewPage(url: String) {
+    AndroidView(factory = { context ->
+        WebView(context).apply {
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    })
+}
