0a46e5d082fb06328b31c5da903f8c92480c6b65
raul monroy <raulisai97@gmail.com>
2025-10-13 22:24:34 -0600

    webView working

--- app/src/main/java/com/example/iam/MainActivity.kt
+++ app/src/main/java/com/example/iam/MainActivity.kt
@@ -1,28 +1,43 @@
 package com.example.iam
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            WebViewPage(url = "https://www.google.com")
+            WebViewPage(url = "https://s8s23kr8-5173.usw3.devtunnels.ms")
         }
     }
 }
 
 @Composable
 fun WebViewPage(url: String) {
-    AndroidView(factory = { context ->
-        WebView(context).apply {
-            webViewClient = WebViewClient()
-            loadUrl(url)
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                settings.javaScriptEnabled = true
+                settings.loadWithOverviewMode = true
+                settings.useWideViewPort = true
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
         }
-    })
+    )
 }
