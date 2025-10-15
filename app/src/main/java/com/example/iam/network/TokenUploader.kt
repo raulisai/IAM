ed142257e57f902841cd1afe07d9ff5b3ee572ff
raul monroy <raulisai97@gmail.com>
2025-10-15 01:31:03 -0600

    version estable de notificaciones

--- app/src/main/java/com/example/iam/network/TokenUploader.kt
+++ app/src/main/java/com/example/iam/network/TokenUploader.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,10 +16,27 @@ object TokenUploader {
     private const val TAG = "TokenUploader"
     // Cambia la baseURL a la de tu backend
     private const val BASE_URL = "https://s8s23kr8-5000.usw3.devtunnels.ms"
+    private const val STATIC_BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNmEwMTI3NzctZmRhZi00ZWUxLWI0MWItYjU5ZjQ4Mzc0ZjU5IiwiZW1haWwiOiJkakB4eC5jb20iLCJuYW1lIjoiRGpva2VyIE0iLCJleHAiOjE3NjA1OTQzNzQsImlhdCI6MTc2MDUwNzk3NH0.q4F1_e6i_zWVnjsUdAHE980Ju71VM1IEK_b3jfIM0TE"
 
     private val api: TokenApi by lazy {
-        val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY } // Usar BODY para mejor depuración
-        val client = OkHttpClient.Builder().addInterceptor(logger).build()
+        // Interceptor para logging
+        val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+        // Interceptor para añadir el token de autorización
+        val authInterceptor = Interceptor { chain ->
+            val originalRequest = chain.request()
+            val requestWithAuth = originalRequest.newBuilder()
+                .header("Authorization", "Bearer $STATIC_BEARER_TOKEN")
+                .build()
+            chain.proceed(requestWithAuth)
+        }
+
+        // Construir el cliente OkHttp con ambos interceptores
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .addInterceptor(authInterceptor)
+            .build()
+
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
